bc98f970c900cb8527929887bad65ab39c705179
Tomislav <tbazina@gmail.com>
2020-02-17 17:39:35 +0100

    Created CRD.

--- linear_quadratic_example.R
+++ linear_quadratic_example.R
@@ -7,15 +7,32 @@ library(dplyr)
 library(magrittr)
 library(purrr)
 library(broom)
+# Visualization
+library(ggplot2)
+# DoE
+library(DoE.wrapper)
 
-# Clean input data
+################################################################################
+# Clean input data (and simulated response)
 dat <- as_tibble(ChickWeight)
 dat <- dat %>% 
   mutate(chick = as.integer(Chick), diet = as.integer(Diet), day = Time) %>%
   group_by(Chick) %>%
   filter(n() >= 12) %>%
   ungroup() %>%
-  select(weight, day, diet, chick) 
+  filter(between(chick, 34, 39) & day %in% c(0, 10, 20)) %>%
+  select(diet, day, weight)
+
+################################################################################
+# CRD - design
+set.seed(42)
+dat <- dat %>%
+  sample_n(n()) %>%
+  mutate(chick = 1:n()) %>%
+  select(chick, day, weight)
+exp.design <- dat %>% select(-weight)
+exp.design %>%
+  write.table(file = 'CRD_Chick_18_3_6.csv', sep = ";", dec = ",")
 
 # EDA
 dat %>% 
@@ -34,6 +51,7 @@ dat %>%
 
 # Visualize
 dat %>%
-  filter(between(chick, 34, 39) & day %in% c(0, 10, 20))
+  filter(between(chick, 34, 39) & day %in% c(0, 10, 20)) %>%
   mutate(chick = factor(chick, ordered=F)) %>%
-  ggplot(aes(day, weight, )) + geom_point() + geom_smooth(method = "lm", formula = y ~ I(x^2))
+  ggplot(aes(day, weight, )) + geom_point() +
+  geom_smooth(method = "lm", formula = y ~ I(x^2))

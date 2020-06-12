b8a5403025ecaadc70cf612ef2a5e217b0870b2e
Tomislav Bazina <tbazina@gmail.com>
2020-06-12 15:35:58 +0200

    Add qq and distribution plots.

--- linear_quadratic_example.R
+++ linear_quadratic_example.R
@@ -11,6 +11,9 @@ library(broom)
 library(ggplot2)
 # DoE
 library(DoE.wrapper)
+# Distribution check
+library(fitdistrplus)
+library(mixtools)
 
 ################################################################################
 # Clean input data (and simulated response)
@@ -39,22 +42,79 @@ exp.design %>%
 ################################################################################
 # EDA
 ## Average, sd
-dat %>% group_by(day) %>% summarise(
-  mean = mean(weight),
-  sd = sd(weight)
-) %>% View()
+mu_sigma <- dat %>% group_by(day) %>% summarise(
+  mu = mean(weight),
+  sigma = sd(weight)
+)
+
+# function for density plot
+sdnorm =
+  function(x, mean=0, sd=1) {
+    dnorm(x, mean=mean, sd=sd)
+    }
+
 ## Histogram
-dat %>%
-  ggplot(aes(x=weight)) +
-  geom_histogram(bins = 5,
+dat <- dat %>% group_by(day) %>% mutate(mu = mean(weight), sd = sd(weight)) %>%
+  ungroup() 
+  
+hist_plot <- function(data) {
+  plt <- ggplot(aes(x=weight, group = day), data = data) +
+  geom_histogram(aes(y = ..density..),
+                 bins = 10,
                  color="black", fill="dodgerblue3",
                  ) +
-  geom_vline(aes(xintercept=mean(weight)), linetype="dashed", size=1) +
+  geom_vline(aes(xintercept=mu), linetype="dashed", size=1) +
+  stat_function(
+    fun = sdnorm,  args = list(
+                  mean = data$mu[1],
+                  sd = data$sd[1]
+                  ),
+    geom = "path", n = 200
+    ) + facet_grid(. ~ day) +
   theme(
     axis.line = element_line(size=0.5, colour = "black")
   )
+  return(plt)
+}
+
+dat %>% filter(day == 0) %>% pull(weight) %>% fitdist('norm') %>% plot(breaks=3)
+
+# Descriptive parameters of distribution
+png('slike/distribution_descriptive.png',
+    width = 14, height = 11, units = 'cm', res = 320, pointsize = 10)
+descdist(data = dat$weight)
+dev.off()
+
+# Normalize data for beta distribution fit
+dat <- dat %>% mutate(
+  weight.norm = (weight - min(weight)) / (max(weight) - min(weight))
+  )
+
+# Fit distribution parameters
+fit.beta <- fitdist(
+  data = dat$weight.norm, distr = 'beta', method = 'qme', probs = c(1/3, 2/3)
+  )
+fit.beta
+png('slike/distribution_hist_qq_cfd_pp.png',
+    width = 16, height = 13, units = 'cm', res = 320, pointsize = 10)
+plot(fit.beta, breaks = 8)
+dev.off()
+
+fit.multimodal <- normalmixEM(
+  dat$weight, lambda = 0.1, mu = mu_sigma$mu, sigma = mu_sigma$sigma)
+plot(fit.multimodal, density = T)
+
+plotdist(dat$weight, breaks = 5, histo = T, demp = T)
   
-qqnorm()
+## Q-Q plot
+dat %>%
+  ggplot(aes(sample=weight)) +
+  stat_qq(
+    shape = 21, colour = "black", fill = "dodgerblue4", size = 2, stroke = 0.3,
+    distribution = function(p) qbeta(p, shape1 = 0.6492259, shape2 = 1.2747630)) +
+  stat_qq_line(distribution = function(p) qbeta(p, shape1 = 0.6492259, shape2 = 1.2747630)) + theme(
+    axis.line = element_line(size=0.5, colour = "black")
+  )
 
 ## DoE Scatter plot
 dat %>% mutate(mean = mean(weight)) %>%
@@ -130,14 +190,7 @@ dat %>%
 ggsave(filename = 'slike/doe_sd_plot.png',
        width = 8, height = 8, units = 'cm', dpi = 320)
 
-## Q-Q plot
-dat %>%
-  ggplot(aes(sample=weight)) +
-  stat_qq(
-    shape = 21, colour = "black", fill = "dodgerblue4", size = 2, stroke = 0.3) +
-  stat_qq_line(distribution = qgeom) + theme(
-    axis.line = element_line(size=0.5, colour = "black")
-  )
+
 
 ## Box-plot
 dat %>% mutate(day = ordered(day)) %>%
